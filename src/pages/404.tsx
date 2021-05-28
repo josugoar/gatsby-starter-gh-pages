aaaa40d1379775f38fb0f409837b61be37defab3
Josu <56826352+josugoar@users.noreply.github.com>
2021-05-28 19:13:47 +0200

    Fixed merge conflict

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,21 +2,12 @@ import { Link } from "gatsby"
 import React from "react"
 
 const NotFoundPage = () => (
-<<<<<<< HEAD
-  <main>
-=======
   <main className="font-mktg">
     <title>Page not found</title>
->>>>>>> f74b74906b3cdbb009924899700e77dd22acdb0c
     <div
       className="position-relative"
       style={{ zIndex: 0, transition: "all 0.25s easi-in" }}
     >
-<<<<<<< HEAD
-      <title>Page not found</title>
-      <h1 style={{ fontSize: 128 }}>404</h1>
-      <p style={{ fontSize: 16 }}>Page not found</p>
-=======
       <div
         className="position-relative d-flex my-0 mx-auto overflow-hidden"
         style={{
@@ -36,7 +27,6 @@ const NotFoundPage = () => (
           Go home
         </Link>
       </div>
->>>>>>> f74b74906b3cdbb009924899700e77dd22acdb0c
     </div>
   </main>
 )
